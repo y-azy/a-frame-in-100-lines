617170367b327bd813b959d6bdb2908e4c749d30
y-azy <25907755+y-azy@users.noreply.github.com>
2025-02-17 11:34:13 -0500

    Second Commit

--- app/page.tsx
+++ app/page.tsx
@@ -25,10 +25,10 @@ const frameMetadata = getFrameMetadata({
 });
 
 export const metadata: Metadata = {
-  title: 'zizzamia.xyz',
+  title: 'Live Life',
   description: 'LFG',
   openGraph: {
-    title: 'zizzamia.xyz',
+    title: 'Live Life',
     description: 'LFG',
     images: [`${NEXT_PUBLIC_URL}/park-1.png`],
   },
